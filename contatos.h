839b59f31a946e9b41836553522203d397defb79
Gabrielly-Bomfim <144944047+Gabrielly-Bomfim@users.noreply.github.com>
2024-04-23 22:51:48 +0000

    array e struct

--- contatos.h
+++ contatos.h
@@ -1,7 +1,12 @@
 //contatos .h = tratamento de erros, struct e array
 
-
-
+#define CONTATOS_LIMITE 255
+struct agenda{
+  char nome[10];
+  char sobrenome [10];
+  char telefone[10];
+  char email [20];
+}CONTATOS_LIMITE; 
 
 
 
