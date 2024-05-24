f2b01a7dd3afaf54fc2b5e63a3483ed1a4a5fc3d
Gabrielly-Bomfim <144944047+Gabrielly-Bomfim@users.noreply.github.com>
2024-05-24 21:30:36 +0000

    contatos_trabalho_pessal

--- contatos.h
+++ contatos.h
@@ -9,12 +9,20 @@ typedef struct {//definindo as variaveis que vamo pedir na função
     char email[20];
 } CONTATO;//definindo o struct
 
-void criando_contatos(CONTATO[], int*);//função para criar contato, com os parametros das variavei e da posição
+typedef struct {//definindo as variaveis que vamo pedir na função
+    char nome[10];
+    char sobrenome[10];
+    char telefone[10];
+    char email[20];
+} CONT_TRABALHO;//definindo o struct
+void criando_contatos(CONTATO[], int*);//função para criar contato
 void salvar(CONTATO[], int*);
 void deletar(CONTATO[], int*);
 void carregar(CONTATO[], int*);
+void trabalho(CONT_TRABALHO[], int*);
 void alterar_dados(CONTATO[], int*);
 void listar(CONTATO[], int*);
+void deletar_trab(CONT_TRABALHO[], int*);
 void clearBuffer();//chamando função limpar buffer
 
 
